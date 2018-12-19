6eeee0c0ae15310c107685d4881140c11da24b22
VS-John <xwen586@sohu.com>
2018-12-19 22:03:13 +0800

    first

---
+++ ptvs/basic/ch01/a01var.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+# expert
+# http://club.topsage.com/thread-361615-1-1.html
+
+# 数字 与 计算
+print( 1/2 )
+print( 1%2 )     #取余 =1
+print( 10 // 3 ) #整除 =3
+print( -3 ** 2 )   # 幂运算
+print( (-3) ** 2 )  
+
+# 16进制，8进制
+print( 0xAF ) # 16进制
+print( 0o10 )  # 8进制： python2下 O10； 
+print( 0b1011010010 )
+
+#字符串
+a = "hello world!a"
+b = '你好！b'
+print(a)
+print(b)
+
+name = "ada lovelace"
+print(name.title())  # 首字母大写
